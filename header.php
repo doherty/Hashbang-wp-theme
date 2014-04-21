bd78683959dd1b6229aeaf9dc39d3430243bd9e0
Mike Doherty <mike@mikedoherty.ca>
2014-04-20 23:05:06 -0300

    Remove Pingdom RUM from <head>
    If you want to use Pingdom's Real User Monitoring service,
    try the Wordpress plugin: https://wordpress.org/plugins/real-user-monitoring-by-pingdom/

--- header.php
+++ header.php
@@ -59,8 +59,6 @@ else {
 
 <link rel="shortcut icon" href="/favicon.ico" />
 
-<script type="application/javascript">var _prum={id:"51670146e6e53da436000000"};var PRUM_EPISODES=PRUM_EPISODES||{};PRUM_EPISODES.q=[];PRUM_EPISODES.mark=function(b,a){PRUM_EPISODES.q.push(["mark",b,a||new Date().getTime()])};PRUM_EPISODES.measure=function(b,a,b){PRUM_EPISODES.q.push(["measure",b,a,b||new Date().getTime()])};PRUM_EPISODES.done=function(a){PRUM_EPISODES.q.push(["done",a])};PRUM_EPISODES.mark("firstbyte");(function(){var b=document.getElementsByTagName("script")[0];var a=document.createElement("script");a.type="text/javascript";a.async=true;a.charset="UTF-8";a.src="//rum-static.pingdom.net/prum.min.js";b.parentNode.insertBefore(a,b)})();</script>
-
 <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 <link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
