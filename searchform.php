bda5d459f287f101c3081c3f629783855cee17f6
Mike Doherty <mike@mikedoherty.ca>
2014-04-26 17:06:05 -0300

    wp_specialchars -> esc_attr

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
         <div>
             <br />
             <label for="s">Search for:</label><br />
-            <input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" size="14" />
+            <input type="text" value="<?php echo esc_attr($s); ?>" name="s" id="s" size="14" />
             <input type="hidden" id="searchsubmit" value="Search" />
         </div>
     </form>
